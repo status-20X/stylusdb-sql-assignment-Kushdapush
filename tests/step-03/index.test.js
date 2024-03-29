5ada37065e7c80ceffdc2cc201644218bd8159dc
Kushagra <22bcs148@iiitdmj.ac.in>
2024-03-29 20:00:06 +0530

    checking the 11th step

--- tests/step-03/index.test.js
+++ tests/step-03/index.test.js
@@ -1,22 +1,26 @@
-const readCSV = require('../../src/csvReader');
-const parseQuery = require('../../src/queryParser');
+const readCSV = require("../../src/csvReader");
+const { parseQuery } = require("../../src/queryParser");
+const executeSELECTQuery = require("../../src/index");
 
-test('Read CSV File', async () => {
-    const data = await readCSV('./student.csv');
+test("Read CSV File", async () => {
+    const data = await readCSV("./student.csv");
     expect(data.length).toBeGreaterThan(0);
-    expect(data.length).toBe(3);
-    expect(data[0].name).toBe('John');
-    expect(data[0].age).toBe('30'); //ignore the string type here, we will fix this later
+    expect(data.length).toBe(4);
+    expect(data[0].name).toBe("John");
+    expect(data[0].age).toBe("30"); //ignore the string type here, we will fix this later
 });
 
-test('Parse SQL Query', () => {
-    const query = 'SELECT id, name FROM student';
+test("Parse SQL Query", () => {
+    const query = "SELECT id, name FROM student";
     const parsed = parseQuery(query);
     expect(parsed).toEqual({
-        fields: ['id', 'name'],
-        table: 'student',
-        "joinCondition": null,
-        "joinTable": null,
-        whereClauses: []
+        fields: ["id", "name"],
+        table: "student",
+        whereClauses: [],
+        joinCondition: null,
+        joinTable: null,
+        joinType: null,
+        groupByFields: null,
+        hasAggregateWithoutGroupBy: false,
     });
 });
